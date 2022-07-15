114e89fa5c3f52a4cd7cd06e85bc455467ff3a15
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2022-07-15 15:04:20 -0400

    add initial page structure using jsx

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = <h1>Testing</h1>;
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(element);
+const page = (
+    <div className='container'>
+        <img src='./react_logo.png' width='100px' />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thosands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+
+)
+
+root.render(page);
