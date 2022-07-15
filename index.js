189e24cd72ec230cd26972b4c5ecef302befad40
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2022-07-15 17:13:18 -0400

    refactor index.js to utilize react components

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const page = (
-    <div className='container'>
-        <img src='./react_logo.png' width='50px' />
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100K stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thosands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
+function Header() {
+    return (
+        <nav>
+            <img src='./react_logo.png' width='50px' />
+        </nav>
+    )
+}
 
-)
+function Footer() {
+    return (
+        <footer>
+            <small>&#169; 2022 Hunnicut Development. All Rights Reserved.</small>
+        </footer>
+    )
+}
 
-root.render(page);
+function MainContent() {
+    return (
+        <div>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thosands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div className='container'>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    
+    )
+} 
+
+root.render(<Page />);
