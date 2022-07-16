6d19c43fe08db264152beb901c2d8b15c39a8c07
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2022-07-16 16:56:57 -0400

    add more robust container divs

--- index.js
+++ index.js
@@ -2,31 +2,46 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function Header() {
     return (
-        <nav>
-            <img src='./react_logo.png' width='50px' />
-        </nav>
+        <header>
+            <div className="content-div">
+                <nav className="main-nav">
+                <img src='./react_logo.png' className="nav-logo" />
+                    <ul className="nav-items">
+                        <li className="nav-item">Pricing</li>
+                        <li className="nav-item">About</li>
+                        <li className="nav-item">Contact</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
     )
 }
 
 function Footer() {
     return (
         <footer>
-            <small>&#169; 2022 Hunnicut Development. All Rights Reserved.</small>
+            <div className="content-div">
+                <div className="footer-content">
+                    <small>&#169; 2022 Jordan Hunnicut Development. All Rights Reserved.</small>
+                </div>
+            </div>
         </footer>
-    )
+        )
 }
 
 function MainContent() {
     return (
-        <div>
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on Github</li>
-                <li>Is maintained by Facebook</li>
-                <li>Powers thosands of enterprise apps, including mobile apps</li>
-            </ul>
+        <div className='main-content'>
+            <div className="content-div">
+                <h1>Fun facts about React</h1>
+                <ul className="react-list">
+                    <li className="react-list--item">Was first released in 2013</li>
+                    <li className="react-list--item">Was originally created by Jordan Walke</li>
+                    <li className="react-list--item">Has well over 100K stars on Github</li>
+                    <li className="react-list--item">Is maintained by Facebook</li>
+                    <li className="react-list--item">Powers thosands of enterprise apps, including mobile apps</li>
+                </ul>
+            </div>
         </div>
     )
 }
